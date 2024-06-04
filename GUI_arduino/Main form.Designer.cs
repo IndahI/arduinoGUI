45a9a8032879060765fc8b31c0523fc53a9bb96b
IndahI <indah.karomi@gmail.com>
2024-06-04 11:31:52 +0700

    uodate system

--- GUI_arduino/Main form.Designer.cs
+++ GUI_arduino/Main form.Designer.cs
@@ -50,8 +50,10 @@ namespace GUI_arduino
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -61,25 +63,28 @@ namespace GUI_arduino
             // scanLabel
             // 
             this.scanLabel.AutoSize = true;
+            this.scanLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scanLabel.Location = new System.Drawing.Point(10, 32);
             this.scanLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.scanLabel.Name = "scanLabel";
-            this.scanLabel.Size = new System.Drawing.Size(53, 20);
+            this.scanLabel.Size = new System.Drawing.Size(55, 24);
             this.scanLabel.TabIndex = 1;
             this.scanLabel.Text = "SCAN";
             // 
             // setLabel
             // 
             this.setLabel.AutoSize = true;
+            this.setLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.setLabel.Location = new System.Drawing.Point(10, 67);
             this.setLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.setLabel.Name = "setLabel";
-            this.setLabel.Size = new System.Drawing.Size(101, 20);
+            this.setLabel.Size = new System.Drawing.Size(103, 24);
             this.setLabel.TabIndex = 2;
             this.setLabel.Text = "SET MODEL";
             // 
             // scanBox
             // 
+            this.scanBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scanBox.Location = new System.Drawing.Point(146, 27);
             this.scanBox.Margin = new System.Windows.Forms.Padding(5);
             this.scanBox.Name = "scanBox";
@@ -89,29 +94,31 @@ namespace GUI_arduino
             // selectedId
             // 
             this.selectedId.AutoSize = true;
+            this.selectedId.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.selectedId.Location = new System.Drawing.Point(143, 106);
             this.selectedId.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.selectedId.Name = "selectedId";
-            this.selectedId.Size = new System.Drawing.Size(14, 20);
+            this.selectedId.Size = new System.Drawing.Size(14, 21);
             this.selectedId.TabIndex = 10;
             this.selectedId.Text = "-";
             // 
             // setModelBox
             // 
             this.setModelBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.setModelBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.setModelBox.FormattingEnabled = true;
             this.setModelBox.Location = new System.Drawing.Point(146, 64);
             this.setModelBox.Margin = new System.Windows.Forms.Padding(5);
             this.setModelBox.Name = "setModelBox";
-            this.setModelBox.Size = new System.Drawing.Size(193, 28);
+            this.setModelBox.Size = new System.Drawing.Size(193, 29);
             this.setModelBox.TabIndex = 11;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.jdgLabel);
-            this.panel1.Location = new System.Drawing.Point(388, 88);
+            this.panel1.Location = new System.Drawing.Point(388, 120);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(263, 236);
             this.panel1.TabIndex = 12;
@@ -119,55 +126,62 @@ namespace GUI_arduino
             // jdgLabel
             // 
             this.jdgLabel.AutoSize = true;
-            this.jdgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 68.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.jdgLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.jdgLabel.Location = new System.Drawing.Point(43, 65);
+            this.jdgLabel.Font = new System.Drawing.Font("Franklin Gothic Demi", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jdgLabel.ForeColor = System.Drawing.Color.White;
+            this.jdgLabel.Location = new System.Drawing.Point(25, 40);
             this.jdgLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.jdgLabel.Name = "jdgLabel";
-            this.jdgLabel.Size = new System.Drawing.Size(177, 102);
+            this.jdgLabel.Size = new System.Drawing.Size(217, 151);
             this.jdgLabel.TabIndex = 13;
             this.jdgLabel.Text = "OK";
+            this.jdgLabel.Visible = false;
             // 
             // judge
             // 
             this.judge.AutoSize = true;
-            this.judge.Location = new System.Drawing.Point(465, 65);
+            this.judge.Font = new System.Drawing.Font("Franklin Gothic Demi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.judge.Location = new System.Drawing.Point(465, 93);
             this.judge.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.judge.Name = "judge";
-            this.judge.Size = new System.Drawing.Size(109, 20);
+            this.judge.Size = new System.Drawing.Size(112, 24);
             this.judge.TabIndex = 13;
             this.judge.Text = "JUDGEMENT";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(10, 104);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.Size = new System.Drawing.Size(126, 24);
             this.label1.TabIndex = 14;
             this.label1.Text = "REG. NUMBER";
             // 
             // portLabel
             // 
             this.portLabel.AutoSize = true;
-            this.portLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.portLabel.Location = new System.Drawing.Point(19, 261);
+            this.portLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portLabel.Location = new System.Drawing.Point(21, 310);
             this.portLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.portLabel.Name = "portLabel";
-            this.portLabel.Size = new System.Drawing.Size(110, 16);
+            this.portLabel.Size = new System.Drawing.Size(116, 21);
             this.portLabel.TabIndex = 15;
             this.portLabel.Text = "PORT ARDUINO";
             // 
             // selectButton
             // 
-            this.selectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectButton.Location = new System.Drawing.Point(156, 279);
+            this.selectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(181)))));
+            this.selectButton.FlatAppearance.BorderSize = 0;
+            this.selectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.selectButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectButton.ForeColor = System.Drawing.Color.White;
+            this.selectButton.Location = new System.Drawing.Point(158, 328);
             this.selectButton.Name = "selectButton";
             this.selectButton.Size = new System.Drawing.Size(77, 28);
             this.selectButton.TabIndex = 17;
             this.selectButton.Text = "OPEN";
-            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.UseVisualStyleBackColor = false;
             this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
             // 
             // portBox
@@ -175,7 +189,7 @@ namespace GUI_arduino
             this.portBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.portBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.portBox.FormattingEnabled = true;
-            this.portBox.Location = new System.Drawing.Point(23, 281);
+            this.portBox.Location = new System.Drawing.Point(25, 330);
             this.portBox.Margin = new System.Windows.Forms.Padding(5);
             this.portBox.Name = "portBox";
             this.portBox.Size = new System.Drawing.Size(125, 24);
@@ -189,40 +203,50 @@ namespace GUI_arduino
             this.groupBox1.Controls.Add(this.scanBox);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.selectedId);
-            this.groupBox1.Location = new System.Drawing.Point(10, 65);
+            this.groupBox1.Location = new System.Drawing.Point(12, 107);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(352, 151);
+            this.groupBox1.Size = new System.Drawing.Size(352, 161);
             this.groupBox1.TabIndex = 19;
             this.groupBox1.TabStop = false;
             // 
             // cancelButton
             // 
-            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cancelButton.Location = new System.Drawing.Point(239, 279);
+            this.cancelButton.BackColor = System.Drawing.Color.Red;
+            this.cancelButton.FlatAppearance.BorderSize = 0;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.ForeColor = System.Drawing.Color.White;
+            this.cancelButton.Location = new System.Drawing.Point(241, 328);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 28);
             this.cancelButton.TabIndex = 20;
             this.cancelButton.Text = "CLOSE";
-            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.UseVisualStyleBackColor = false;
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(181)))));
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(20, 45);
-            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(225, 20);
+            this.label3.Size = new System.Drawing.Size(217, 21);
             this.label3.TabIndex = 22;
             this.label3.Text = "Laundry System Business Unit";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(17, 14);
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(181)))));
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(17, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(304, 29);
+            this.label2.Size = new System.Drawing.Size(334, 37);
             this.label2.TabIndex = 24;
             this.label2.Text = "Barcode Scanner Case A";
             // 
@@ -230,19 +254,29 @@ namespace GUI_arduino
             // 
             this.label5.AutoSize = true;
             this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
-            this.label5.Location = new System.Drawing.Point(634, 346);
+            this.label5.Location = new System.Drawing.Point(634, 378);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(17, 20);
             this.label5.TabIndex = 25;
             this.label5.Text = "  ";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(181)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(663, 74);
+            this.panel2.TabIndex = 26;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 374);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(663, 408);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.groupBox1);
@@ -251,6 +285,7 @@ namespace GUI_arduino
             this.Controls.Add(this.portLabel);
             this.Controls.Add(this.judge);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -262,6 +297,8 @@ namespace GUI_arduino
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -287,6 +324,7 @@ namespace GUI_arduino
         private Label label3;
         private Label label2;
         private Label label5;
+        private Panel panel2;
     }
 }
 
--- GUI_arduino/Main form.cs
+++ GUI_arduino/Main form.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO.Ports;
 using System.Windows.Forms;
@@ -7,17 +9,33 @@ namespace GUI_arduino
 {
     public partial class Form1 : Form
     {
+        //Variabel yang digunakan
+        private Color defaultColor;
+        private string barcodeData, jdgData, latestReceivedData = "", result;
+        private string connectionString = "Data Source=DESKTOP-66EIFJP\\SQLEXPRESS;Initial Catalog=LSBU_Packing;Integrated Security=True;TrustServerCertificate=True";
+        private bool isKeyboardEnabled = true;
         private ToolTip toolTip;
+
+        private int check = 0;
+
         public Form1()
         {
             //SETUP
-
             InitializeComponent();
             toolTip = new ToolTip();
+
             //Mengatur trigger key untuk scanner
             this.KeyPreview = true;
-            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
             this.Focus();
+        }
+
+        // Definisi kelas Model
+        public class Model
+        {
+            public string Date { get; set; }
+            public string ModelNumber { get; set; }
+            public string SerialNumber { get; set; }
+            public string Result { get; set; }
         }
 
         //array data
@@ -28,21 +46,18 @@ namespace GUI_arduino
             "NA-W90B1 (GRG)", "NA-W120G1 (VRT)", "NA-W50B1 (W)", "NA-W1000B (WSA)", "NA-W800B (H)",
             "NA-W120G1 (H)", "NA-W120G1 (HTW)"};
 
+        //8887549777585
+
         private string[] posNumber = {
-            "8887549777585","8887549777608", "8887549777622", "8887549777646", "8887549777660",
+            "9780201379624","8887549777608", "8887549777622", "8887549777646", "8887549777660",
             "8887549736711", "8887549720987", "8887549720970", "8887549720963", "8887549829253",
             "8887549829260", "8887549829277", "8887549664939", "8887549664953", "8887549664946",
             "8887549664960", "8887549779909", "4549980490655", "8887549788413", "8887549788420",
             "8887549762475", "4713170842931", };
 
-        //Variabel yang digunakan
-        private Color defaultColor;
-        private string barcodeData, jdgData, latestReceivedData;
-        private bool isKeyboardEnabled = true;
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            toolTip.SetToolTip(label5, "Copyright © 2024 Indah & Yahya | PENS & UNSURYA");
+            toolTip.SetToolTip(label5, "Copyright © 2024 PENS & UNSURYA");
             string[] ports = SerialPort.GetPortNames();
             portBox.Items.AddRange(ports);
 
@@ -72,16 +87,55 @@ namespace GUI_arduino
             if (jdgData == barcodeData)
             {
                 serialPort1.Write("OK"); //Mengirim data ke serial
+                jdgLabel.Visible = true;
                 jdgLabel.Text = "OK";
+                result = "OK";
                 BeginInvoke(new Action(() => OKpanel_ChangeColor(Color.Green)));
+                CreateModel();
             }
             else if (jdgData != barcodeData)
             {
                 serialPort1.Write("NG");//Mengirim data ke serial
+                jdgLabel.Visible = true;
                 jdgLabel.Text = "NG";
+                result = "NG";
                 BeginInvoke(new Action(() => NGpanel_ChangeColor(Color.Red)));
+                CreateModel();
             }
+        }
 
+        public void CreateModel()
+        {
+            DateTime currentDate = DateTime.Now; // Tanggal dan waktu saat tombol ditekan
+            string formattedDate = currentDate.ToString("yyyy-MM-dd HH:mm:ss"); // Format tanggal dan waktu
+
+            var model = new Model
+            {
+                Date = formattedDate,
+                ModelNumber = setModelBox.Text,
+                SerialNumber = jdgData,
+                Result = result
+            };
+
+            AddData(model);
+
+        }
+
+        public void AddData(dynamic model)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+
+                command.CommandText = "INSERT INTO Packing_Results (date, model_number, serial_number, result) values (@Date, @ModelNumber, @SerialNumber, @Result)";
+                command.Parameters.Add("@Date", SqlDbType.DateTime).Value = model.Date;
+                command.Parameters.Add("@ModelNumber", SqlDbType.VarChar).Value = model.ModelNumber;
+                command.Parameters.Add("@SerialNumber", SqlDbType.VarChar).Value = model.SerialNumber;
+                command.Parameters.Add("@Result", SqlDbType.VarChar).Value = model.Result;
+                command.ExecuteNonQuery();
+            }
         }
 
         //Mengubah nilai enumerasi Keys ke representasi string yang sesuai.
@@ -98,6 +152,7 @@ namespace GUI_arduino
                 serialPort1.DataBits = serialPort1.DataBits;
                 serialPort1.StopBits = serialPort1.StopBits;
                 serialPort1.Open();
+                MessageBox.Show("Connected to serial port");
                 scanBox.Focus();
             }
             catch (Exception ex)
@@ -111,6 +166,8 @@ namespace GUI_arduino
             if (serialPort1.IsOpen)
             {
                 serialPort1.Close();
+                MessageBox.Show("Disconnected from serial port");
+                portBox.Text = "";
             }
         }
 
@@ -122,24 +179,26 @@ namespace GUI_arduino
                 // Periksa apakah tombol yang ditekan adalah tombol Enter (dengan asumsi pemindai barcode mengirimkan tombol Enter setelah pemindaian)
                 if (e.KeyCode == Keys.Enter)
                 {
+                    e.SuppressKeyPress = true; // Suppress further key presses
                     BeginInvoke(new Action(() =>
                     {
                         string latestReceivedData = scanBox.Text;
                         isKeyboardEnabled = false;
                         jdgData = latestReceivedData.Trim(); //Menghapus /r atau /n (whitespace) dari variabel
+                        latestReceivedData = ""; // Clear latestReceivedData after processing
                         judgement();
                     }));
                 }
                 else
                 {
-                    //Akumulasi karakter hingga tombol Enter ditekan
+                    // Akumulasi karakter hingga tombol Enter ditekan
                     string character = keysConverter.ConvertToString(e.KeyCode);
                     latestReceivedData += character;
                 }
             }
             else
             {
-                e.SuppressKeyPress = true;//input keyboard ditolak
+                e.SuppressKeyPress = true; // Input keyboard ditolak
             }
         }
 
@@ -158,13 +217,13 @@ namespace GUI_arduino
 
                 // Mengatur timer untuk mengembalikan warna kembali ke default setelah waktu tertentu
                 Timer revertTimer = new Timer();
-                revertTimer.Interval = 1500; // delay 2 detik
-                revertTimer.Start(); // Timer menyala
+                revertTimer.Interval = 1500; // delay 1.5 detik
+                revertTimer.Start(); // Timer menyal
                 revertTimer.Tick += (sender, e) =>
                 {
                     panel1.BackColor = defaultColor; // Mengembalikan ke warna default
                     scanBox.Clear();
-                    jdgLabel.Text="";
+                    jdgLabel.Text = "";
                     revertTimer.Stop(); // Timer mati
                     isKeyboardEnabled = true; //Mengembalikan ke loop scan agar bisa menerima input keyboard kembali
                 };
